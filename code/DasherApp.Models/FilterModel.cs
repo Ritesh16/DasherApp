0ae3a1b263f24032ef9202926ad541a2130d7b3a
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-04-04 22:08:47 -0400

    Adding option for searching without dates

--- code/DasherApp.Models/FilterModel.cs
+++ code/DasherApp.Models/FilterModel.cs
@@ -2,13 +2,14 @@
 {
     public class FilterModel
     {
+        public bool SearchWithoutDates { get; set; }
         public DateTime FromDate { get; set; }
         public DateTime ToDate { get; set; }
-
         public string Location { get; set; }
 
         public FilterModel()
         {
+            SearchWithoutDates = false;
             ToDate = DateTime.Now;
             FromDate = new DateTime(ToDate.Year, ToDate.Month, 1);
         }
